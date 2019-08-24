cd59304a32b72519f6f48cda7a8a2586a6256daa
JokerY <770792058@qq.com>
2019-08-24 20:38:58 +0800

    Add level-2 echo featuer

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,7 @@
 package seedu.duke;
 
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -8,5 +10,17 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
+        Duke.echo();
+    }
+
+    private static void echo(){
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while(!input.equals("bye")){
+            System.out.println(input);
+            input = scanner.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again!");
     }
 }
